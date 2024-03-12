0adfe35bfd5916dc9b8142c42525510f84fe7202
Sergio Loza <sergiolozav@gmail.com>
2024-03-12 02:30:59 -0400

    style: Prettified con tailwind

--- src/modules/components/NavbarPublic/components/ToggleNavbarButton.tsx
+++ src/modules/components/NavbarPublic/components/ToggleNavbarButton.tsx
@@ -3,7 +3,7 @@ export function ToggleNavbarButton() {
     <button
       data-collapse-toggle="navbar-default"
       type="button"
-      className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+      className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
       aria-controls="navbar-default"
       aria-expanded="false"
     >
