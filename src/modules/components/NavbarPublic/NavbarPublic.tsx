c15a62c8b912dd51689696091a8ac69b9963464d
Sergio Loza <sergiolozav@gmail.com>
2024-03-12 02:29:17 -0400

    feat: Agregada altura máxima a navbar

--- src/modules/components/NavbarPublic/NavbarPublic.tsx
+++ src/modules/components/NavbarPublic/NavbarPublic.tsx
@@ -4,14 +4,14 @@ import { ToggleNavbarButton } from "./components/ToggleNavbarButton";
 
 export function NavbarPublic() {
   return (
-    <nav className="border-gray-200">
+    <nav className="max-h-16">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <CompanyBrand />
         <ToggleNavbarButton />
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse">
             <li>
-              <LinkOption text="App" to="/" />
+              <LinkOption text="App" to="/map" />
             </li>
             <li>
               <LinkOption text="Información" to="/login" />
