f1142329e84bca96691ea64bb4056518f05a6131
Sergio Loza <sergiolozav@gmail.com>
2024-03-12 02:28:44 -0400

    feat: Imágen temporal de navbar reemplazada

--- src/modules/components/NavbarPublic/components/CompanyBrand.tsx
+++ src/modules/components/NavbarPublic/components/CompanyBrand.tsx
@@ -5,7 +5,7 @@ export function CompanyBrand() {
       className="flex items-center space-x-3 rtl:space-x-reverse"
     >
       <img
-        src="https://flowbite.com/docs/images/logo.svg"
+        src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
         className="h-8"
         alt="Flowbite Logo"
       />
