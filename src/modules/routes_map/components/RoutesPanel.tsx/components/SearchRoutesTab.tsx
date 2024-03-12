90853ea2c4bf7627d76f36981a5a937df2913055
Sergio Loza <sergiolozav@gmail.com>
2024-03-12 09:31:48 -0400

    feat: Diseño base

--- src/modules/routes_map/components/RoutesPanel.tsx/components/SearchRoutesTab.tsx
+++ src/modules/routes_map/components/RoutesPanel.tsx/components/SearchRoutesTab.tsx
@@ -1,20 +1,46 @@
+import { FcHome, FcSearch } from "react-icons/fc";
+import { RouteSuggestionItem } from "../../RouteSuggestionItem/RouteSuggestionItem";
+
 export function SearchRoutesTab() {
+  const columnClassName = "flex flex-col gap-3 px-5";
   return (
-    <div
-      className="flex flex-col gap-3 bg-stone-600 px-6 py-4"
-      style={{
-        background:
-          "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-      }}
-    >
-      <input
-        className="rounded-md px-2.5 py-1.5 outline-none"
-        placeholder="Elige un punto de inicio"
-      />
-      <input
-        className="rounded-md px-2.5 py-1.5 outline-none"
-        placeholder="Elige un destino"
-      />
+    <div className="flex flex-col py-6">
+      <div className={columnClassName}>
+        <div className="flex items-center rounded-md border border-stone-200/80 bg-stone-100 px-2">
+          <FcSearch />
+          <input
+            className="bg-inherit py-1.5 pl-2 text-sm outline-none"
+            placeholder="Punto de inicio"
+          />
+        </div>
+        <div className="flex items-center rounded-md border border-stone-200/80 bg-stone-100 px-2">
+          <FcHome />
+          <input
+            className="bg-inherit py-1.5 pl-2 text-sm outline-none"
+            placeholder="Destino"
+          />
+        </div>
+      </div>
+      <div className="mt-4 h-[1px] w-full bg-stone-300"></div>
+      <div className={columnClassName}>
+        <p className="mt-3">Rutas recomendadas</p>
+        <RouteSuggestionItem
+          time={5}
+          steps={[
+            { type: "Walk", data: 5 },
+
+            { type: "Bus", data: 38 },
+          ]}
+        />
+        <div className="h-[1px] w-full bg-stone-300"></div>
+        <RouteSuggestionItem
+          time={11}
+          steps={[
+            { type: "Bus", data: 42 },
+            { type: "Bus", data: 38 },
+          ]}
+        />
+      </div>
     </div>
   );
 }
