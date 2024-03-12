d967d3bdf079c37477c172b7f5fb395323605dac
Sergio Loza <sergiolozav@gmail.com>
2024-03-12 07:03:42 -0400

    feat: Creada página de buscador de rutas

--- src/modules/routes_map/RoutesMapPage.tsx
+++ src/modules/routes_map/RoutesMapPage.tsx
@@ -1,7 +1,13 @@
+import { RoutesMap } from "./components/RoutesMap";
+import { RoutesPanel } from "./components/RoutesPanel.tsx/RoutesPanel";
+
 export function RoutesMapPage() {
   return (
-    <div className="">
-      
+    <div className="flex min-h-[calc(100vh-4rem)] w-full flex-col">
+      <div className="relative flex h-full flex-1 flex-col">
+        <RoutesPanel />
+        <RoutesMap />
+      </div>
     </div>
   );
 }
