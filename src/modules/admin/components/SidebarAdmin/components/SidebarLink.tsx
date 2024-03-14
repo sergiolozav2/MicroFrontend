ed41f6c10c580550f06a5abdd13bf12c036db1e0
Sergio Loza <sergiolozav@gmail.com>
2024-03-14 04:19:28 -0400

    feat: Agregado borde al estar activo

--- src/modules/admin/components/SidebarAdmin/components/SidebarLink.tsx
+++ src/modules/admin/components/SidebarAdmin/components/SidebarLink.tsx
@@ -9,13 +9,13 @@ interface SidebarOptionButton extends LinkProps {
 export function SidebarLink(props: SidebarOptionButton) {
   return (
     <Link
-      className="[&.active]:bg-primary-100 [&.active]:text-primary-600 rounded-md py-2 pl-3 pr-4 text-left text-stone-600 hover:bg-stone-900/5"
+      className="[&.active]:bg-primary-100 [&.active]:border-primary-600 [&.active]:text-primary-600 rounded-md border-l-4 border-transparent py-2 pl-3 pr-4 text-left text-stone-600 hover:bg-stone-900/5"
       to={props.to}
       onClick={props.onClick}
     >
       <div className="flex items-center text-sm">
         <span className="text-lg">{props.icon}</span>
-        <h3 className="pl-4">{props.text}</h3>
+        <h3 className="pl-3">{props.text}</h3>
       </div>
     </Link>
   );
