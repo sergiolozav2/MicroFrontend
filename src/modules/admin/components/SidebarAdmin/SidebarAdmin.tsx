33e470c696ecaebcf1a5ae6e83b8f54307fbed19
Sergio Loza <sergiolozav@gmail.com>
2024-03-14 14:33:10 -0400

    fix: Sidebar tapaba a contenido de módulo

--- src/modules/admin/components/SidebarAdmin/SidebarAdmin.tsx
+++ src/modules/admin/components/SidebarAdmin/SidebarAdmin.tsx
@@ -13,17 +13,17 @@ import { SidebarTitle } from "./components/SidebarTitle";
 
 export function SidebarAdmin() {
   return (
-    <div className="fixed inset-0 right-auto flex min-h-screen w-admin-navbar flex-col">
-      <div className="bg-primary-50 hidden items-center border-r border-stone-300 px-4 py-3 md:flex">
+    <div className="pointer-events-none fixed inset-0 right-auto flex min-h-screen w-admin-navbar flex-col md:pointer-events-auto">
+      <div className="hidden items-center border-r border-stone-300 bg-white px-4 py-3 md:flex">
         <img className="mr-2 max-w-6" src="/logo-bus.svg" />
-        <p className="text-primary-950 font-semibold">Micro</p>
+        <p className="font-semibold text-primary-950">Micro</p>
       </div>
       <div className="flex h-9 items-end p-1 md:hidden">
-        <button className="px-1 py-0.5 text-xl hover:bg-stone-900/10 ">
+        <button className="pointer-events-auto px-1 py-0.5 text-xl hover:bg-stone-900/10 ">
           <MdMenu />
         </button>
       </div>
-      <div className="from-primary-50 to-primary-300 h-full origin-left scale-x-0 border-r border-stone-300 bg-gradient-to-b from-40% pt-4 shadow-md transition-transform md:scale-x-100">
+      <div className="h-full origin-left scale-x-0 border-r border-stone-300 bg-gradient-to-b from-white from-80% to-primary-300/60 pt-4 shadow-md transition-transform md:scale-x-100">
         <div className="flex flex-col gap-1 px-2 py-1 font-medium">
           <SidebarTitle text="Aplicaciones" />
           <SidebarLink
