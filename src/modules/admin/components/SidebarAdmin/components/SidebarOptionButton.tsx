aaa8c300b285ceb4d104c9861aac319de36b8280
Sergio Loza <sergiolozav@gmail.com>
2024-03-13 23:41:57 -0400

    feat: Reducido contraste de fondo con hover

--- src/modules/admin/components/SidebarAdmin/components/SidebarOptionButton.tsx
+++ src/modules/admin/components/SidebarAdmin/components/SidebarOptionButton.tsx
@@ -9,7 +9,7 @@ interface SidebarOptionButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function SidebarOptionButton(props: SidebarOptionButton) {
   return (
     <button
-      className="rounded-md py-2 pl-3 pr-4 text-left text-stone-600 hover:bg-stone-900/10"
+      className="rounded-md py-2 pl-3 pr-4 text-left text-stone-600 hover:bg-stone-900/5"
       onClick={props.onClick}
     >
       <div className="flex items-center text-sm">
