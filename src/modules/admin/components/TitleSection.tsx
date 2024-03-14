2aae46a70307155a32bc1df80c6b3da07c88f134
Sergio Loza <sergiolozav@gmail.com>
2024-03-14 14:31:13 -0400

    feat: Componente para sección de título de modulo

--- src/modules/admin/components/TitleSection.tsx
+++ src/modules/admin/components/TitleSection.tsx
@@ -1,7 +1,14 @@
 type PageTitleProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export function TitleSection(props: PageTitleProps) {
-  return <div className="ml-10 md:ml-0 flex h-9 md:h-fit items-center">{props.children}</div>;
+  return (
+    <div
+      className={`ml-10 mr-2 mt-[1px] flex min-h-9 items-center font-medium md:ml-8 md:h-fit md:py-4 md:pr-4 ${props.className}`}
+    >
+      {props.children}
+    </div>
+  );
 }
