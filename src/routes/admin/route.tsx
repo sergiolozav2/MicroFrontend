b50c389bae32150c8cfb01c7521a06533f7e124c
Sergio Loza <sergiolozav@gmail.com>
2024-03-13 23:31:11 -0400

    feat: Layout para aplicación de admin

---
+++ src/routes/admin/route.tsx
@@ -0,0 +1,20 @@
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { NavbarAdmin } from "../../modules/components_admin/NavbarAdmin/NavbarAdmin";
+import { SidebarAdmin } from "../../modules/components_admin/SidebarAdmin/SidebarAdmin";
+
+export const Route = createFileRoute("/admin")({
+  component: AppLayout,
+});
+
+function AppLayout() {
+  return (
+    <>
+      <NavbarAdmin />
+      <SidebarAdmin />
+      <div className="flex">
+        <div className="md:mr-admin-navbar mr-0 transition-all"></div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
